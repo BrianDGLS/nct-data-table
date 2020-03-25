8ebcb6a66140e54fb67517096e1451826659ff9f
BrianDGLS <briandgls92@gmail.com>
2020-03-25 18:33:36 +0000

    feature: added scss stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 import { NCTDataService, PassFailRate } from './NCTDataService'
 import { VehicleReport } from './VehicleReport'
 
